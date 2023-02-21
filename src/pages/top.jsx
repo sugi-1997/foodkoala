567dfb9b64d98a6a81194e32635934a83fd0f6b3
hiroki_horikawa <horikawa.hiroki0820@gmail.com>
2023-02-21 14:58:50 +0900

    トップページの作成

---
+++ src/pages/top.jsx
@@ -0,0 +1,29 @@
+import Head from "next/head"
+import Link from "next/link";
+
+
+export default function TopPage() {
+    return(
+        <>
+      <Head>
+        <title>Food Koala トップ</title>
+      </Head>
+      <div style={{ textAlign: 'center', marginTop: '2em' }}>
+        <h1>美味しい料理をお得に食べられて、フードロスも減らせる!!</h1>
+      </div>
+      <br/>
+      <div style={{ textAlign: 'center', marginTop: '2em' }}>
+        <p>テキスト詳細</p>
+      </div>
+      <div style={{ textAlign: 'center', marginTop: '2em' }}> 
+        <p>画像挿入</p>
+      </div>
+      <div style={{ textAlign: 'center', marginTop: '2em' }}>
+        <button><Link href='/userRegist'>新規会員登録</Link></button>
+      </div>
+
+      </>
+    )
+
+  }
+  
