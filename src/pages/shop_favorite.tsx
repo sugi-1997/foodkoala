1c1ab0c82298e19f9577d237f1080af29adbd974
sugiurasaki <saki.sugiura@rakus-partners.co.jp>
2023-02-22 18:03:49 +0900

    shop_list_css

--- src/pages/shop_favorite.tsx
+++ src/pages/shop_favorite.tsx
@@ -1,14 +1,45 @@
 import ShopName from '../components/shop_name';
 import ShopMenu from '../components/shop_menu';
+import styles from '../styles/Shop.module.css';
+import Head from 'next/head';
 
 export default function ShopFavorite() {
   return (
     <>
-      <div className="shopFavorite_name">
-        <ShopName />
-      </div>
-      <div className="shopFavorite_menu">
-        <ShopMenu />
+      <Head>
+        <title>お気に入り店舗一覧</title>
+      </Head>
+      <div className={styles.shopFavorite}>
+        <div className={styles.shopFavorite_shop}>
+          <div className={styles.shopFavorite_name}>
+            <ShopName />
+          </div>
+          <div className={styles.shopFavorite_menu}>
+            <ShopMenu />
+            <ShopMenu />
+            <ShopMenu />
+          </div>
+        </div>
+        <div className={styles.shopFavorite_shop}>
+          <div className={styles.shopFavorite_name}>
+            <ShopName />
+          </div>
+          <div className={styles.shopFavorite_menu}>
+            <ShopMenu />
+            <ShopMenu />
+            <ShopMenu />
+          </div>
+        </div>
+        <div className={styles.shopFavorite_shop}>
+          <div className={styles.shopFavorite_name}>
+            <ShopName />
+          </div>
+          <div className={styles.shopFavorite_menu}>
+            <ShopMenu />
+            <ShopMenu />
+            <ShopMenu />
+          </div>
+        </div>
       </div>
     </>
   );
--- src/pages/shop_list.tsx
+++ src/pages/shop_list.tsx
@@ -1,14 +1,45 @@
 import ShopName from '../components/shop_name';
 import ShopMenu from '../components/shop_menu';
+import styles from '../styles/Shop.module.css';
+import Head from 'next/head';
 
 export default function ShopList() {
   return (
     <>
-      <div className="shopList_name">
-        <ShopName />
-      </div>
-      <div className="shopList_menu">
-        <ShopMenu />
+      <Head>
+        <title>お店一覧</title>
+      </Head>
+      <div className={styles.shopList}>
+        <div className={styles.shopList_shop}>
+          <div className={styles.shopList_name}>
+            <ShopName />
+          </div>
+          <div className={styles.shopList_menu}>
+            <ShopMenu />
+            <ShopMenu />
+            <ShopMenu />
+          </div>
+        </div>
+        <div className={styles.shopList_shop}>
+          <div className={styles.shopList_name}>
+            <ShopName />
+          </div>
+          <div className={styles.shopList_menu}>
+            <ShopMenu />
+            <ShopMenu />
+            <ShopMenu />
+          </div>
+        </div>
+        <div className={styles.shopList_shop}>
+          <div className={styles.shopList_name}>
+            <ShopName />
+          </div>
+          <div className={styles.shopList_menu}>
+            <ShopMenu />
+            <ShopMenu />
+            <ShopMenu />
+          </div>
+        </div>
       </div>
     </>
   );
