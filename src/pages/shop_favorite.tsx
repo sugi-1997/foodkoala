5c0b49e2eaec11d94048ee7fe824439d32b87af1
sugiurasaki <saki.sugiura@rakus-partners.co.jp>
2023-02-24 12:14:05 +0900

    shop_list

--- src/pages/shop_favorite.tsx
+++ src/pages/shop_favorite.tsx
@@ -2,6 +2,10 @@ import ShopName from '../components/shop_name';
 import ShopMenu from '../components/shop_menu';
 import styles from '../styles/Shop.module.css';
 import Head from 'next/head';
+import Header from '@/components/header';
+import Genre from '@/components/genre';
+import Area from '@/components/area';
+import Footer from '@/components/footer';
 
 export default function ShopFavorite() {
   return (
@@ -9,7 +13,10 @@ export default function ShopFavorite() {
       <Head>
         <title>お気に入り店舗一覧</title>
       </Head>
-      <div className={styles.shopFavorite}>
+      <main className={styles.shopFavorite}>
+        <Header />
+        <Genre />
+        <Area />
         <div className={styles.shopFavorite_shop}>
           <div className={styles.shopFavorite_name}>
             <ShopName />
@@ -20,27 +27,8 @@ export default function ShopFavorite() {
             <ShopMenu />
           </div>
         </div>
-        <div className={styles.shopFavorite_shop}>
-          <div className={styles.shopFavorite_name}>
-            <ShopName />
-          </div>
-          <div className={styles.shopFavorite_menu}>
-            <ShopMenu />
-            <ShopMenu />
-            <ShopMenu />
-          </div>
-        </div>
-        <div className={styles.shopFavorite_shop}>
-          <div className={styles.shopFavorite_name}>
-            <ShopName />
-          </div>
-          <div className={styles.shopFavorite_menu}>
-            <ShopMenu />
-            <ShopMenu />
-            <ShopMenu />
-          </div>
-        </div>
-      </div>
+        <Footer />
+      </main>
     </>
   );
 }
