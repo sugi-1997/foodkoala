f347a73ebe054b4b443d86f55e66b18f1b341c90
sugiurasaki <saki.sugiura@rakus-partners.co.jp>
2023-03-07 11:55:49 +0900

    css

--- src/pages/concept.tsx
+++ src/pages/concept.tsx
@@ -11,29 +11,31 @@ export default function TopPage() {
     <>
       <Head>
         <title>Food Koala について</title>
+        <script
+          src="https://kit.fontawesome.com/acecca202b.js"
+          crossOrigin="anonymous"
+        ></script>
       </Head>
-      
+
       <Header />
-      
+
       <div className={styles.concept}>
-      
-      {/* パンくずリスト */}
-      <BreadList list={[menu_list, concept]} />
-      
+        {/* パンくずリスト */}
+        <BreadList list={[menu_list, concept]} />
 
-      {/*画像・理念・コアラの島*/}
+        {/*画像・理念・コアラの島*/}
 
-      <div className='firstBlock'>
-      <div className={styles.concept_food}>
-      <Image
-          src="/images/menu/hamburger.food.jpg"
-          alt="concept"
-          width={400}
-          height={250}
-          className={styles.concept_food}
-        />
-        </div>
-        {/*<div className={styles.concept_text}>
+        <div className="firstBlock">
+          <div className={styles.concept_food}>
+            <img
+              src="/images/menu/hamburger.food.jpg"
+              alt="concept"
+              width={400}
+              height={250}
+              className={styles.concept_food}
+            />
+          </div>
+          {/*<div className={styles.concept_text}>
         <h2>いつまでも「おいしい」に包まれた世界へ</h2>
         <h3>
           foodKoalaは廃棄寸前の食品専門のテイクアウト予約サービス。
@@ -43,144 +45,158 @@ export default function TopPage() {
           「おいしい」に包まれた未来を守ります。
         </h3>
   </div> */}
-        
-        <div>
-        {/* <Image
+
+          <div>
+            {/* <Image
           src="/images/foodkoala_img2.PNG"
           alt="concept"
           width={100}
           height={100}
           className={styles.concept_koala}
         /> */}
+          </div>
         </div>
-        </div>
-        
 
         {/* 中段 */}
-        
-        <div className={styles.flex_box}>
-       
-        <div className={styles.box}>
-        <div className={styles.Img}>
-        <Image
-          src="/images/foodkoala_img2.PNG"
-          alt="concept"
-          width={100}
-          height={100}
-        />
+        <div className={styles.about}>
+          <h2>Food Koalaについて</h2>
+          <div className={styles.flex_box}>
+            <div className={styles.box}>
+              <div className={styles.Img}>
+                <img
+                  src="/images/concept/waste.concept.png"
+                  alt="concept"
+                  width={100}
+                  height={100}
+                />
+              </div>
+              <div className={styles.text}>
+                <h3>食品ロス削減に貢献！</h3>
+                <p>日本国内における食品ロスは年間600万トン。</p>
+                <p>
+                  うち、外食産業による食品ロスは全体の16%。※H30年度
+                </p>
+                <p>FoodKoalaの利用で、</p>
+                <p>食品廃棄物削減の社会貢献をすることができます。</p>
+              </div>
+            </div>
+
+            <div className={styles.box}>
+              <div className={styles.Img}>
+                <img
+                  src="/images/concept/eat.concept.png"
+                  alt="concept"
+                  width={100}
+                  height={100}
+                />
+                <img src="" alt="" />
+              </div>
+              <div className={styles.text}>
+                <h3>おいしい店舗たくさん！</h3>
+                <p>Food Koalaの登録店舗のジャンルは様々。</p>
+                <p>
+                  気になっていたお店のお試しにもご利用いただけます。
+                </p>
+                <p>あなたのお気に入りなお店も見つかるかも！</p>
+                <p>おいしく楽しい食事の時間を提供します。</p>
+              </div>
+            </div>
+
+            <div className={styles.box}>
+              <div className={styles.Img}>
+                <img
+                  src="/images/concept/money.concept.png"
+                  alt="concept"
+                  width={100}
+                  height={100}
+                />
+              </div>
+              <div className={styles.text}>
+                <h3>環境への配慮でお得に！</h3>
+                <p>廃棄予定の食品を販売しているため、</p>
+                <p>
+                  すべてのメニューが通常よりお得な金額となっております。
+                </p>
+                <p>さらに、容器の持参で</p>
+                <p>地球にやさしくお得な割引も行っています。</p>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className={styles.text}>
-          <h3>ごみ</h3>
-          <p>
-            日本国内における食品ロスは年間600万トンにのぼります。
-            平成30年度中でも、外食産業による食品ロスは全体の16%を占め、
-            約81万トンもの食品廃棄物を排出しています。
-            FoodKoalaの利用で、食品廃棄物削減の社会貢献をすることができます。
-          </p>
+
+        {/* 下段 */}
+        <div className={styles.howTo}>
+          <h2>ご利用方法</h2>
+          <div className={styles.flex_box}>
+            <div className={styles.box}>
+              <div className={styles.Img}>
+                <img
+                  src="/images/concept/search.concept.png"
+                  alt="concept"
+                  width={100}
+                  height={100}
+                />
+              </div>
+              <div className={styles.text}>
+                <br />
+                <i className="fa-solid fa-1"></i>
+                <h3>商品を探す</h3>
+              </div>
+            </div>
+
+            <div className={styles.box}>
+              <div className={styles.Img}>
+                <img
+                  src="/images/concept/cart.concept.png"
+                  alt="concept"
+                  width={100}
+                  height={100}
+                />
+              </div>
+              <div className={styles.text}>
+                <br />
+                <i className="fa-solid fa-2"></i>
+                <h3>商品を予約する</h3>
+              </div>
+            </div>
+
+            <div className={styles.box}>
+              <div className={styles.Img}>
+                <img
+                  src="/images/concept/get.concept.png"
+                  alt="concept"
+                  width={100}
+                  height={100}
+                />
+              </div>
+              <div className={styles.text}>
+                <br />
+                <i className="fa-solid fa-3"></i>
+                <h3>お店で商品を受け取る</h3>
+              </div>
+            </div>
+          </div>
         </div>
-        </div>
 
-        <div className={styles.box}>
-        <div className={styles.Img}>
-        <Image
-          src="/images/foodkoala_img2.PNG"
-          alt="concept"
-          width={100}
-          height={100}
-        />
-        </div>
-        <div className={styles.text}>
-          <h3>おいしいお店</h3>
-          <p>Food Koalaの登録店舗のジャンルは様々。
-             気になっていたお店のお試しにもご利用いただけます。
-             あなたのお気に入りなお店も見つかるかも！
-             おいしく楽しい食事の時間を提供します。
-          </p>
-        </div>
-        </div>
-
-        <div className={styles.box}>
-        <div className={styles.Img}>
-        <Image
-          src="/images/foodkoala_img2.PNG"
-          alt="concept"
-          width={100}
-          height={100}
-        />
-        </div>
-        <div className={styles.text}>
-          <h3>お得</h3>
-          <p>廃棄予定の食品を販売しているため、すべてのメニューが通常よりお得な金額となっております。
-             さらに、容器の持参で地球にやさしくお得な割引も行っています。
-          </p>
-        </div>
-        </div>
-        </div>
-
-          {/* 下段 */}
-        
-      <div className={styles.flex_box}>
-       
-       <div className={styles.box}>
-       <div className={styles.Img}>
-       <Image
-         src="/images/foodkoala_img2.PNG"
-         alt="concept"
-         width={100}
-         height={100}
-       />
-       </div>
-       <div className={styles.text}>
-         <h3>商品を探す</h3>
-       </div>
-       </div>
-
-       <div className={styles.box}>
-       <div className={styles.Img}>
-       <Image
-         src="/images/foodkoala_img2.PNG"
-         alt="concept"
-         width={100}
-         height={100}
-       />
-       </div>
-       <div className={styles.text}>
-         <h3>商品を予約する</h3>
-       </div>
-       </div>
-
-       <div className={styles.box}>
-       <div className={styles.Img}>
-       <Image
-         src="/images/foodkoala_img2.PNG"
-         alt="concept"
-         width={100}
-         height={100}
-       />
-       </div>
-       <div className={styles.text}>
-         <h3>お店で行って商品を受け取る</h3>
-       </div>
-       </div>
-       </div>
-
-
-
-
-
-
-
-
-          
-        
         {/* 新規会員登録へ */}
-        <div className={styles.concept_goToUserResister}>
-          <Link href="/userResisterPage">
-            <input type="button" value='新規会員登録'/>
-            </Link>
+        <div className={styles.start}>
+          <h2>\&nbsp;&nbsp;いますぐ始める&nbsp;&nbsp;/</h2>
+          <div className={styles.start_flex_box}>
+            <div className={styles.start_box}>
+              <a href="/userResisterPage">
+                会員登録はこちら&nbsp;
+                <i className="fa-solid fa-users"></i>
+              </a>
+            </div>
+            <div className={styles.start_box}>
+              <a href="/">
+                商品一覧はこちら&nbsp;
+                <i className="fa-solid fa-cart-shopping"></i>
+              </a>
+            </div>
+          </div>
         </div>
-        
-        </div>
+      </div>
       <Footer />
     </>
   );
