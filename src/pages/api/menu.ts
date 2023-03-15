3079dfc7c6e7f5afb534e010162fc40f6d7fbb29
yukatakahashi <yuka.takahashi@rakus-partners.co.jp>
2023-03-15 14:43:16 +0900

    committed

--- src/pages/api/menu.ts
+++ src/pages/api/menu.ts
@@ -1,5 +1,4 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-// import fetch from 'isomorphic-unfetch';
 
 export default async function ItemData(
   req: NextApiRequest,
@@ -8,10 +7,15 @@ export default async function ItemData(
   const genreId = req.query.genreId;
   const areaId = req.query.areaId;
   const id = req.query.id;
+  const url = process.env['NEXT_PUBLIC_SUPABASE_URL'];
   if (id!.includes('eq')) {
     try {
-      const url = process.env['BACKEND_API_URL'];
-      const response = await fetch(`${url}/items?id=${id}`);
+      const response = await fetch(`${url}/items?id=${id}`, {
+        headers: {
+          apikey: `${process.env['NEXT_PUBLIC_SUPABASE_ANON_KEY']}`,
+          Authorization: `Bearer ${process.env['NEXT_PUBLIC_SUPABASE_ANON_KEY']}`,
+        },
+      });
       const data = await response.json();
       if (!response.ok) {
         throw new Error('Fail to Load...');
@@ -21,12 +25,16 @@ export default async function ItemData(
       }
       res.status(200).json(data);
     } catch (error) {
-      res.status(400).json({ error: error });
+      res.status(400).json({ error: error.message });
     }
   } else if (areaId!.includes('eq')) {
     try {
-      const url = process.env['BACKEND_API_URL'];
-      const response = await fetch(`${url}/items?area_id=${areaId}`);
+      const response = await fetch(`${url}/items?area_id=${areaId}`, {
+        headers: {
+          apikey: `${process.env['NEXT_PUBLIC_SUPABASE_ANON_KEY']}`,
+          Authorization: `Bearer ${process.env['NEXT_PUBLIC_SUPABASE_ANON_KEY']}`,
+        },
+      });
       const data = await response.json();
       if (!response.ok) {
         throw new Error('Fail to Load...');
@@ -36,13 +44,18 @@ export default async function ItemData(
       }
       res.status(200).json(data);
     } catch (error) {
-      res.status(400).json({ error: error });
+      res.status(400).json({ error: error.message });
     }
   } else {
     try {
-      const url = process.env['BACKEND_API_URL'];
       const response = await fetch(
-        `${url}/items?genre_id=${genreId}`
+        `${url}/items?genre_id=${genreId}`,
+        {
+          headers: {
+            apikey: `${process.env['NEXT_PUBLIC_SUPABASE_ANON_KEY']}`,
+            Authorization: `Bearer ${process.env['NEXT_PUBLIC_SUPABASE_ANON_KEY']}`,
+          },
+        }
       );
       const data = await response.json();
       if (!response.ok) {
@@ -53,7 +66,7 @@ export default async function ItemData(
       }
       res.status(200).json(data);
     } catch (error) {
-      res.status(400).json({ error: error });
+      res.status(400).json({ error: error.message });
     }
   }
 }
