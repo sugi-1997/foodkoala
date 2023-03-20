1c5a6347ea476874ab6d165ea4cbf6aebe046eab
sugiurasaki <saki.sugiura@rakus-partners.co.jp>
2023-03-20 14:51:41 +0900

    review from DB

--- src/components/option.tsx
+++ src/components/option.tsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from 'react';
 import styles from 'styles/order_check.module.css';
 import type { CartItem } from 'types/cart_item';
-import { userId } from 'lib/UserId';
+import Cookies from 'js-cookie';
 
 export default function Option() {
   const [itemId, setItemId] = useState<CartItem[]>([]);
+  const userId = Cookies.get('user_id');
 
   //cart_itemsテーブルからデータを取得
   useEffect(() => {
